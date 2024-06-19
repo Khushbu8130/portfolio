696ccac56c125a9d9075bdc815f6bec73b908928
KHUSHBU KUMARI <131510344+Khushbu8130@users.noreply.github.com>
2024-06-19 12:39:19 +0530

    Add files via upload

---
+++ My portfolio/script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Section visibility on scroll
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+    observer.observe(section);
+})
